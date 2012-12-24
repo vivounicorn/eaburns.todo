b0c890cd75730d3f6ef9702a14c180101ef251f3
Ethan Burns <burns.ethan@gmail.com>
2012-12-23 22:52:29 -0500

    Completely re-write the Item interface.

--- todo/todo.go
+++ todo/todo.go
@@ -1,264 +1,143 @@
 // © 2012 Ethan Burns under the MIT license.
 
-// Todo is a package for reading, modifying, and writing todo.txt files.
 package todo
 
 import (
-	"sort"
 	"strings"
 	"time"
 	"unicode"
 	"unicode/utf8"
 )
 
-// PriorityRunes are the valid priority runes.
-const PriorityRunes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	// ProjectMarker is the rune that begins a project name.
+	projectMarker = '+'
 
-type priority rune
+	// ContextMarker is the rune that begins a context name.
+	contextMarker = '@'
 
-// NoPriority is the priority constant for no priority
-const NoPriority priority = priority(0)
+	// KeywordSep is the rune separating a keyword/value binding.
+	keywordSep = ':'
 
-// Priority returns a priority value for the given rune.  If the rune is
-// not a valid priority then it panics.
-func Priority(r rune) priority {
-	if !strings.ContainsRune(PriorityRunes, r) {
-		panic("Invalid priority: " + string(r))
-	}
-	return priority(r)
-}
+	// PrioRunes is a string of all valid priority runes.
+	prioRunes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// String returns the string representation of the priority.
-func (p priority) String() string {
-	if p == NoPriority {
-		return ""
-	}
-	return "(" + string(p) + ")"
-}
+	// DateFormat is the format string for dates.
+	dateFormat = "2006-01-02"
+)
 
-// A File is a sequence of todo items.
-type File []Item
-
-// An Item represents a single line of a todo file.
+// An Item is a single line of a todo.txt file.
 type Item struct {
-	// Priority is the priority string for this item.
-	Priority priority
-
-	// Projects is a list of projects for this item without
-	// the leading + marker.
-	Projects []string
-
-	// Contexts is a list of the contexts for this item without
-	// the leading @ marker.
-	Contexts []string
-
-	// Done is true if this item is completed.
-	Done bool
-
-	// AddedDate and FinishedDate are the added and finished
-	// dates of this item or they are the zero time if the item
-	// does not have the corresponding date.
-	AddedDate, FinishDate time.Time
-
-	// Text is the text of the item (excluding completion info,
-	// priority, and added date prefix) at the time that it was
-	// parsed, or at the last call to Text().
-	text string
+	LineNo int
+	Text   string
 }
 
-// ParseItem returns an item parsed from a line, or an error
-// if the line is not a valid item.
-func ParseItem(line string) *Item {
-	i := new(Item)
-	i.Done, i.FinishDate, line = parseCompleted(line)
-	i.Priority, line = parsePriority(line)
-	i.AddedDate, line = parseDate(line)
-	i.text = line
-	i.Projects = findTags('+', line)
-	i.Contexts = findTags('@', line)
-	return i
+// Done returns true if the item is marked as done.
+func (item *Item) Done() bool {
+	d, _ := parseDone(item.Text)
+	return d
 }
 
-// timeFormat is the time format required by todo.txt.
-const timeFormat = "2006-01-02 "
-
-// ParseCompleted returns the done status, finish time and the rest
-// of the string.
-func parseCompleted(s string) (bool, time.Time, string) {
-	if len(s) >= 2 && s[0] == 'x' && s[1] == ' ' {
-		t, s := parseDate(s[2:])
-		return true, t, s
-	}
-	return false, time.Time{}, s
+// Priority returns the priority string for this item.
+func (item *Item) Priority() string {
+	_, prio, _, _ := item.Header()
+	return prio
 }
 
-// ParseDate returns a date parsed from the beginning of the string
-// and the remainder of the string.  If the string does not begin with
-// a valid date then the zero time is returned along with the input string.
-func parseDate(s string) (time.Time, string) {
-	if len(s) < len(timeFormat) {
-		return time.Time{}, s
+// Header returns the header information from the item.
+func (item *Item) Header() (done bool, prio string, doneDate, addDate time.Time) {
+	text := item.Text
+	done, text = parseDone(text)
+	if done {
+		doneDate, text = parseDate(text)
 	}
-	t, err := time.Parse(timeFormat, s[:len(timeFormat)])
-	if err != nil {
-		return time.Time{}, s
-	}
-	return t, s[len(timeFormat):]
-}
-
-// ParsePriority returns a priority value from the beginning of the string
-// and the remainder of the string.  If there is no priority value at the
-// beginning of the string, then PriorityNone is returned along with the
-// input string.
-func parsePriority(s string) (priority, string) {
-	if len(s) < 4 || s[0] != '(' || !strings.ContainsRune(PriorityRunes, rune(s[1])) || s[2] != ')' || s[3] != ' ' {
-		return priority('\000'), s
-	}
-	return priority(s[1]), s[4:]	// discard the space
-}
-
-// FindTags returns all tags in the string that begin with the given marker.
-func findTags(marker rune, s string) (tags []string) {
-	for {
-		start := strings.IndexRune(s, marker)
-		if start < 0 {
-			break
-		}
-		s = s[start+1:]
-
-		end := strings.IndexFunc(s, unicode.IsSpace)
-		if end < 0 {
-			end = len(s)
-		}
-
-		tag := s[:end]
-		if len(tag) > 1 {
-			l, _ := utf8.DecodeLastRuneInString(tag)
-			if unicode.IsDigit(l) || unicode.IsLetter(l) || l == '_' {
-				tags = append(tags, tag)
-			}
-		}
-	}
+	prio, text = parsePriority(text)
+	addDate, _ = parseDate(text)
 	return
 }
 
-// Text returns the item text, which is the string representation
-// of the item excluding any completed, priority, and date prefix.
-func (i *Item) Text() string {
-	i.text = reconsileTags(i.text, '+', i.Projects)
-	i.text = reconsileTags(i.text, '@', i.Contexts)
-	i.text = strings.TrimSpace(i.text)
-	return i.text
+// ParseDone returns the completed status from the string and the
+// rest of the string after it.
+func parseDone(s string) (bool, string) {
+	if len(s) >= 2 && s[0] == 'x' && s[1] == ' ' {
+		return true, s[2:]
+	}
+	return false, s
 }
 
-// ReconsileTags returns a new copy of the given text with
-// tags added if they are in the slice but not the text and tags
-// removed if they are in the text but no the slice.
-func reconsileTags(text string, marker rune, tags []string) string {
-	inSlice := make(map[string]bool)
-	for _, t := range tags {
-		inSlice[t] = true
+// ParseDate returns the time from the beginning of the string, or the
+// zero time if the string doesn't begin with a time, and the rest of the
+// string.
+func parseDate(s string) (time.Time, string) {
+	if len(s) < len(dateFormat) {
+		return time.Time{}, s
 	}
-
-	// Remove tags not in the slice, longest first.
-	inText := make(map[string]bool)
-	for _, t := range findTags(marker, text) {
-		inText[t] = true
+	t, err := time.Parse(dateFormat, s[:len(dateFormat)])
+	if err != nil {
+		return time.Time{}, s
 	}
-	sort.Sort(longFirstSorter(tags))
-	for t := range inText {
-		if !inSlice[t] {
-			text = rmTag(text, string(marker)+t)
-		}
+	s = s[len(dateFormat):]
+	if len(s) >= 1 && s[0] == ' ' {
+		s = s[1:]
 	}
-
-	// See what tags are still in the text, and add all remaining ones
-	inText = make(map[string]bool)
-	for _, t := range findTags(marker, text) {
-		inText[t] = true
-	}
-	for _, t := range tags {
-		if !inText[t] {
-			text = addTag(text, string(marker)+t)
-		}
-	}
-
-	return text
+	return t, s
 }
 
-type longFirstSorter []string
-
-func (l longFirstSorter) Len() int {
-	return len(l)
-}
-
-func (l longFirstSorter) Swap(i, j int) {
-	l[i], l[j] = l[j], l[i]
-}
-
-func (l longFirstSorter) Less(i, j int) bool {
-	return len(l[i]) > len(l[j])
-}
-
-// AddTag adds the tag to the string, inserting a space if necessary.
-func addTag(s, tag string) string {
-	l, _ := utf8.DecodeLastRuneInString(s)
-	if !unicode.IsSpace(l) {
-		return s + " " + tag
+// ParsePriority parses a priority value from the string and returns it and
+// the rest of the string. If the string doesn't begin with a priority then
+// an empty string.
+func parsePriority(s string) (string, string) {
+	if len(s) < 3 || s[0] != '(' || !strings.ContainsRune(prioRunes, rune(s[1])) || s[2] != ')' {
+		return "", s
 	}
-	return s + tag
+	prio := s[1:2]
+	s = s[3:]
+	if len(s) >= 1 && s[0] == ' ' {
+		s = s[1:]
+	}
+	return prio, s
 }
 
-// RmTag removes the tag from the string.  This is pretty inefficient.
-func rmTag(s, tag string) string {
-	rest := s
-	s = ""
-	space := true
-	for len(rest) > 0 {
-		if ok, sz := tagPrefix(rest, tag); ok {
-			if !space {	// Not preceeded by a space, don't cut the space
-				sz = len(tag)
-			}
-			rest = rest[sz:]
+// Projects returns a slice of all projects for this item.
+func (item *Item) Projects() []string {
+	return item.tags(projectMarker)
+}
+
+// Contexts returns a slice of all contexts for this item.
+func (item *Item) Contexts() []string {
+	return item.tags(contextMarker)
+}
+
+// Tags returns all tags in the text of the item that begin with the
+// given marker.
+func (item *Item) tags(marker rune) []string {
+	var tags []string
+	for _, f := range strings.Fields(item.Text) {
+		if first, _ := utf8.DecodeRuneInString(f); first != marker {
 			continue
 		}
-		r, w := utf8.DecodeRuneInString(rest)
-		s += string(r)
-		space = unicode.IsSpace(r)
-		rest = rest[w:]
-	}
-	return s
-}
-
-// TagPrefix returns true if the prefix is the tag.
-func tagPrefix(s, tag string) (bool, int) {
-	if !strings.HasPrefix(s, tag) {
-		return false, 0
-	}
-	if len(s) == len(tag) {
-		return true, len(s)
-	}
-	if r, w := utf8.DecodeRuneInString(s[len(tag):]); unicode.IsSpace(r) {
-		return true, len(tag)+w
-	}
-	return false, 0
-}
-
-// String returns the string representation of an Item.
-func (i *Item) String() string {
-	s := ""
-	if i.Done {
-		s += "x "
-		if !i.FinishDate.IsZero() {
-			s += i.FinishDate.Format(timeFormat)
+		if last, _ := utf8.DecodeLastRuneInString(f); !tagEnd(last) {
+			continue
 		}
+		tags = append(tags, f)
 	}
-	if i.Priority != NoPriority {
-		s += i.Priority.String() + " "
-	}
-	if !i.AddedDate.IsZero() {
-		s += i.AddedDate.Format(timeFormat)
-	}
-	return strings.TrimSpace(s + i.Text())
+	return tags
 }
 
+// TagEnd returns true for runes that are valid tag ends.
+func tagEnd(r rune) bool {
+	return unicode.IsDigit(r) || unicode.IsLetter(r) || r == '_'
+}
+
+// Keywords returns a mapping of all <keyword>:<value> pairs in this item.
+func (item *Item) Keywords() map[string]string {
+	kwds := make(map[string]string)
+	for _, f := range strings.Fields(item.Text) {
+		i := strings.IndexRune(f, keywordSep)
+		if i < 0 {
+			continue
+		}
+		kwds[f[:i]] = f[i+1:]
+	}
+	return kwds
+}
--- todo/todo_test.go
+++ todo/todo_test.go
@@ -1,3 +1,5 @@
+// © 2012 Ethan Burns under the MIT license.
+
 package todo
 
 import (
@@ -7,342 +9,64 @@ import (
 	"time"
 )
 
-func TestParseTime(t *testing.T) {
-	tests := []struct{
-		text string
-		time time.Time
-		rest string
+func TestHeader(t *testing.T) {
+	tests := []struct {
+		text              string
+		done              bool
+		prio              string
+		doneTime, addTime time.Time
 	}{
-		{ "", time.Time{}, "" },
-		{ "Foo bar", time.Time{}, "Foo bar" },
-		{ "2012-12-23 ", d(2012, time.December, 23), "" },
-		{ "2012-12-23 Hello", d(2012, time.December, 23), "Hello" },
+		{"", false, "", time.Time{}, time.Time{}},
+		{"x ", true, "", time.Time{}, time.Time{}},
+		{"x 2012-12-23", true, "", d(2012, time.December, 23), time.Time{}},
+		{"x 2012-12-23 (A)", true, "A", d(2012, time.December, 23), time.Time{}},
+		{"x 2012-12-23 (A) 2012-12-20", true, "A", d(2012, time.December, 23), d(2012, time.December, 20)},
+		{"2012-12-23 (A) 2012-12-20", false, "", time.Time{}, d(2012, time.December, 23)},
+		{"x (A) 2012-12-20", true, "A", time.Time{}, d(2012, time.December, 20)},
+		{"x 2012-12-23 2012-12-20", true, "", d(2012, time.December, 23), d(2012, time.December, 20)},
 	}
 	for _, test := range tests {
-		time, rest := parseDate(test.text)
-		if !time.Equal(test.time) {
-			t.Errorf("Text [%s], expected time [%s], got [%s]", test.text, test.time, time)
+		item := &Item{Text: test.text}
+		done, prio, doneTime, addTime := item.Header()
+		if done != test.done {
+			t.Errorf("Text [%s] expected done %t, got %t", test.text, test.done, done)
 		}
-		if rest != test.rest {
-			t.Errorf("Text [%s], expected rest [%s], got [%s]", test.text, test.rest, rest)
+		if prio != test.prio {
+			t.Errorf("Text [%s] expected prio %s, got %s", test.text, test.prio, prio)
 		}
-	}
-}
-
-func TestParsePriority(t *testing.T) {
-	tests := []struct{
-		text, prio, rest string
-	}{
-		{ "(A) ", "(A)", "" },
-		{ "(a) ", "", "(a) " },
-		{ "(B) Hello", "(B)", "Hello" },
-		{ "(B", "", "(B" },
-		{ "", "", "" },
-		{ "Really gotta call Mom (A) @phone @someday", "", "Really gotta call Mom (A) @phone @someday" },
-		{ "(b) Get back to the boss", "", "(b) Get back to the boss" },
-		{ "(B)->Submit TPS report", "", "(B)->Submit TPS report" },
-	}
-	for _, test := range tests {
-		prio, rest := parsePriority(test.text)
-		if prio.String() != test.prio {
-			t.Errorf("Text [%s], expected priority [%s], got [%s]", test.text, test.prio, prio)
+		if !doneTime.Equal(test.doneTime) {
+			t.Errorf("Text [%s] expected doneTime %s, got %s", test.text, test.doneTime, doneTime)
 		}
-		if rest != test.rest {
-			t.Errorf("Text [%s], expected rest [%s], got [%s]", test.text, test.rest, rest)
+		if !addTime.Equal(test.addTime) {
+			t.Errorf("Text [%s] expected addTime %s, got %s", test.text, test.addTime, addTime)
 		}
 	}
 }
 
-func TestFindTags(t *testing.T) {
-	tests := []struct{
-		text string
-		marker rune
-		tags []string
-	}{
-		{ "", '+', nil },
-		{ "(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone", '+', []string{"Family", "PeaceLoveAndHappiness"} },
-		{ "(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone", '@', []string{"iphone", "phone"} },
-		{ "+foo+bar", '+', []string{ "foo+bar", "bar" } },
-		{ "+foo+bar+", '+', nil },
-		{ "++foo+bar", '+', []string{ "+foo+bar", "foo+bar", "bar" } },
-	}
-
-	for _, test := range tests {
-		tags := findTags(test.marker, test.text)
-		sort.Strings(test.tags)
-		sort.Strings(tags)
-		if !reflect.DeepEqual(test.tags, tags) {
-			t.Errorf("Text [%s], expected tags %v, got %v", test.text, test.tags, tags)
-		}
-	}
-}
-
-func TestParseItem(t *testing.T) {
-	tests := []struct{
-		text string
-		item Item
-	}{
-		{ "xhello", Item{
-			text: "xhello",
-		} },
-
-		{ "x hello", Item{
-			text: "hello",
-			Done: true,
-		} },
-
-		{ "x 2012-12-23 hello", Item{
-			text: "hello",
-			Done: true,
-			FinishDate: d(2012, time.December, 23),
-		} },
-
-		{ "x 2012-12-23 2012-12-20 hello", Item{
-			text: "hello",
-			Done: true,
-			AddedDate: d(2012, time.December, 20),
-			FinishDate: d(2012, time.December, 23),
-		} },
-
-		{ "2012-12-23 2012-12-20 hello", Item{
-			text: "2012-12-20 hello",
-			AddedDate: d(2012, time.December, 23),
-		} },
-
-		{ "(A) 2012-12-23 hello", Item{
-			text: "hello",
-			Priority: Priority('A'),
-			AddedDate: d(2012, time.December, 23),
-		} },
-
-		{ "(A) 2012-12-23 +hello", Item{
-			text: "+hello",
-			Priority: Priority('A'),
-			Projects: []string{"hello"},
-			AddedDate: d(2012, time.December, 23),
-		} },
-
-		{ "(A) +hello", Item{
-			text: "+hello",
-			Priority: Priority('A'),
-			Projects: []string{"hello"},
-		} },
-
-		{ "(A) +hello @goodbye Hi there", Item{
-			text: "+hello @goodbye Hi there",
-			Priority: Priority('A'),
-			Contexts: []string{"goodbye"},
-			Projects: []string{"hello"},
-		} },
-
-		{ "(A) +hello +goodbye Hi there", Item{
-			text: "+hello +goodbye Hi there",
-			Priority: Priority('A'),
-			Projects: []string{"hello", "goodbye"},
-		} },
-
-		{ "x 2011-03-02 2011-03-01 Review Tim's pull request +TodoTxtTouch @github", Item {
-			text: "Review Tim's pull request +TodoTxtTouch @github",
-			Done: true,
-			FinishDate: d(2011, time.March, 2),
-			AddedDate: d(2011, time.March, 1),
-			Projects: []string{"TodoTxtTouch"},
-			Contexts: []string{ "github" },
-		} },
-
-		{ "(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone", Item{
-			text: "Call Mom +Family +PeaceLoveAndHappiness @iphone @phone",
-			Priority: Priority('A'),
-			Projects: []string{"Family", "PeaceLoveAndHappiness"},
-			Contexts: []string{ "iphone", "phone"},
-		} },
-	}
-	for _, test := range tests {
-		item := ParseItem(test.text)
-		if !itemEq(item, &test.item) {
-			t.Errorf("Text [%s], expected\n%#v,\ngot\n%#v", test.text, test.item, *item)
-		}
-	}
-}
-
-func TestRmTags(t *testing.T) {
-	tests := []struct{
-		text string
-		tags []string
-		result string
-	}{
-		{ "+foo bar", []string{"+foo"}, "bar"},
-		{ "+foo +bar", []string{"+foo"}, "+bar"},
-		{ "+foo +bar", []string{"+bar"}, "+foo "},
-		{ "+foo+bar", []string{"+foo"}, "+foo+bar"},
-		{ "foo+bar", []string{"+bar"}, "foo"},
-	}
-	for _, test := range tests {
-		text := test.text
-		for _, tag := range test.tags {
-			text = rmTag(text, tag)
-		}
-		if text != test.result {
-			t.Errorf("Text [%s], removing %v, expected [%s], got [%s]",
-				test.text, test.tags, test.result, text)
-		}
-	}
-}
-
-func TestItem_TextAndString(t *testing.T) {
-	tests := []struct{
-		item Item
-		str, txt string
-	}{
-		{
-			Item{
-				text: "hello",
-				Priority: Priority('A'),
-			},
-			"(A) hello",
-			"hello",
-		},
-		{
-			Item{
-				text: "hello +there",
-				Priority: Priority('A'),
-				Projects: []string{"there"},
-			},
-			"(A) hello +there",
-			"hello +there",
-		},
-		{
-			Item{
-				text: "hello +there",
-				Priority: Priority('A'),
-				Projects: []string{"there", "foo"},
-			},
-			"(A) hello +there +foo",
-			"hello +there +foo",
-		},
-		{
-			Item{
-				text: "hello +there ",
-				Priority: Priority('A'),
-				Projects: []string{"there", "foo"},
-			},
-			"(A) hello +there +foo",
-			"hello +there +foo",
-		},
-		{
-			Item{
-				text: "hello @there",
-				Priority: Priority('A'),
-				Contexts: []string{"there"},
-			},
-			"(A) hello @there",
-			"hello @there",
-		},
-		{
-			Item{
-				text: "hello @there ",
-				Priority: Priority('A'),
-				Contexts: []string{"there", "foo"},
-			},
-			"(A) hello @there @foo",
-			"hello @there @foo",
-		},
-		{
-			Item{
-				Done: true,
-				text: "hello @there ",
-				Priority: Priority('A'),
-				Contexts: []string{"there", "foo"},
-			},
-			"x (A) hello @there @foo",
-			"hello @there @foo",
-		},
-		{
-			Item{
-				Done: true,
-				FinishDate: d(2012, time.December, 23),
-				text: "hello @there ",
-				Priority: Priority('A'),
-				Contexts: []string{"there", "foo"},
-			},
-			"x 2012-12-23 (A) hello @there @foo",
-			"hello @there @foo",
-		},
-		{
-			Item{
-				Done: true,
-				FinishDate: d(2012, time.December, 23),
-				AddedDate: d(2012, time.December, 20),
-				text: "hello @there ",
-				Priority: Priority('A'),
-				Contexts: []string{"there", "foo"},
-			},
-			"x 2012-12-23 (A) 2012-12-20 hello @there @foo",
-			"hello @there @foo",
-		},
-		{
-			Item{
-				text: "+foo @bar +baz @bazm",
-				Priority: Priority('A'),
-				Projects: []string{"foo"},
-				Contexts: []string{"bar"},
-			},
-			"(A) +foo @bar",
-			"+foo @bar",
-		},
-		{
-			Item{
-				text: "+foo+bar +baz",
-				Priority: Priority('A'),
-				Projects: []string{"foo"},
-			},
-			"(A) +foo",
-			"+foo",
-		},
-		{
-			Item{
-				text: "+foo+bar",
-				Projects: []string{"bar"},
-			},
-			"+bar",
-			"+bar",
-		},
-		{
-			Item{
-				text: "a +foo+bar	b	",
-				Projects: []string{"bar"},
-			},
-			"a b	+bar",
-			"a b	+bar",
-		},
-	}
-	for _, test := range tests {
-		if txt := test.item.Text(); txt != test.txt {
-			t.Errorf("Item %v\nexpected text [%s],\ngot [%s]", test.item, test.txt, txt)
-		}
-		if str := test.item.String(); str != test.str {
-			t.Errorf("Item %v\nexpected text [%s],\ngot [%s]", test.item, test.str, str)
-		}
-		
-	}
-}
-
-// ItemEq returns true if the two items are equal.
-func itemEq(a, b *Item) bool {
-	sort.Strings(a.Contexts)
-	sort.Strings(b.Contexts)
-	sort.Strings(a.Projects)
-	sort.Strings(b.Projects)
-	return a.text == b.text &&
-		a.Priority == b.Priority &&
-		reflect.DeepEqual(a.Projects, b.Projects) &&
-		reflect.DeepEqual(a.Contexts, b.Contexts) &&
-		a.Done == b.Done &&
-		a.AddedDate.Equal(b.AddedDate) &&
-		a.FinishDate.Equal(b.FinishDate)
-}
-
-// D returns a date.
 func d(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 0, 0, 0, 0, time.FixedZone("UTC", 0))
+}
+
+func TestTags(t *testing.T) {
+	tests := []struct {
+		text   string
+		marker rune
+		tags   []string
+	}{
+		{"", '+', nil},
+		{"+foo +bar", '+', []string{"+foo", "+bar"}},
+		{"@foo @bar", '@', []string{"@foo", "@bar"}},
+		{"hello +foo there +bar", '+', []string{"+foo", "+bar"}},
+		{"hello @foo there @bar", '@', []string{"@foo", "@bar"}},
+		{"+foo+ +bar", '+', []string{"+bar"}},
+	}
+	for _, test := range tests {
+		item := &Item{Text: test.text}
+		tags := item.tags(test.marker)
+		sort.Strings(tags)
+		sort.Strings(test.tags)
+		if !reflect.DeepEqual(tags, test.tags) {
+			t.Errorf("Text [%s], marker %c expected %v, got %v", test.text, test.marker, test.tags, tags)
+		}
+	}
 }
